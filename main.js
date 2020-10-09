474f4b0dbbdb7f7cf9b438a1df5cf275a2c2debf
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-09 19:51:51 +0200

    space beetwen fucnions and correct errors

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 let $inputMain; // main input on sites
 let $alert; // alert to add tasks
 let $addMain; // main button to add tasks below
-let $tasksMain; //tasks below input and button
+let $tasksMainUL; //tasks below input and button
 let $newTasks; // NEW ADD ELEMENT TASKS
 
 
@@ -16,7 +16,7 @@ const downloadEle = () => {
      $inputMain = document.querySelector(".todo");
       $alert = document.querySelector(".tex1");
        $addMain = document.querySelector(".smain");
-        $tasksMain = document.querySelector("ul");
+        $tasksMainUL = document.querySelector("ul");
 };
 
 const listenerEle = () => {
@@ -26,7 +26,7 @@ const addNewTasks = () => {
 if($inputMain.value !== ''){
          $newTasks = document.createElement("li");
          $newTasks.innerText = $inputMain.value;
-         $tasksMain.appendChild($newTasks);
+         $tasksMainUL.appendChild($newTasks);
 
          $inputMain.value = '';
          $alert.innerText = '';
